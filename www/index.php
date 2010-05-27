4f26334810ed6f47f47f2c1759477b8f7108fcaf
hoehle (Michael Höhle) <hoehle@math.su.se>
2010-05-27 20:44:09 +0000

    updated index
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,26 +1,48 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!-- This is the project specific website template -->
+<!-- It can be changed as liked or replaced by other content -->
+
+<?php
+
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-<head>
-<!-- This is the project specific website template --><!-- It can be changed as liked or replaced by other content -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>&lt;?php echo $group_name; ?&gt;</title>
-  <link href="%3C?php%20echo%20$themeroot;%20?%3Estyles/estilo1.css"
- rel="stylesheet" type="text/css">
-</head>
+
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php echo $group_name; ?></title>
+	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  </head>
+
 <body>
-';
-?&gt;<!-- --- R-Forge Logo --- -->
-<table border="0" cellpadding="0" cellspacing="0" width="100%">
-  <tbody>
-    <tr>
-      <td><a href="/"><img
- src="%3C?php%20echo%20$themeroot;%20?%3E/images/logo.png"
- alt="R-Forge Logo" border="0"> </a> </td>
-    </tr>
-  </tbody>
+
+<! --- R-Forge Logo --- >
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
+
+
 <!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like --><!-- end of project description -->
+<!-- own website starts here, the following may be changed as you like -->
+
+<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+$contents = '';
+while (!feof($handle)) {
+	$contents .= fread($handle, 8192);
+}
+fclose($handle);
+echo $contents; } ?>
+
+<!-- end of project description -->
+
 <hr><b>Description:</b>
 <ul>
   <ul>
