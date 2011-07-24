03acc0a1a18556415057ab2db584709a57b1bbc2
hoehle (Michael Höhle) <hoehle@math.su.se>
2011-07-24 17:57:31 +0000

    changed scan of hypGeomSmall.txt into a call of data with unwrapping of data.frame to vector of numerics as the scan failed to build on Rforge. Not sure why, but maybe the path statement did not work here...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -9,12 +9,16 @@
   library(msm)
 
   #Load the CIdata thing
-  data(CIdata, package=pkgname)
+  data("CIdata", package=pkgname)
 
   #Read the table of the hypgeom_2F1 function for parameters c(1/3,2/3) and
   #5/3 -- atm this is computed for the values seq(0,10,by=0.01) and 11:100
   #Load the pre-evaluated Hypergeometric function for computing Anscombe residuals
-  surveillance.gvar.hyp <<- scan(file.path(.path.package('surveillance'),'data',"hypGeomSmall.txt"),quiet=TRUE)
+#Apparently, this fails on R-Forge.  Use "data" instead.
+#  surveillance.gvar.hyp <<- scan(file.path(.path.package('surveillance'),'data',"hypGeomSmall.txt"),quiet=TRUE)
+  data("hypGeomSmall",package="surveillance")
+  surveillance.gvar.hyp <<- hypGeomSmall$hypgeom2F1
+  
   surveillance.gvar.z <<- - c(0:1000/100, 11:100)
 
   #Load the C code library for the glr stuff
