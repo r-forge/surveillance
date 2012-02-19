7b89874ef0990faf0b228bf0219f64fac2df7d08
hoehle (Michael Höhle) <hoehle@math.su.se>
2012-02-19 14:35:26 +0000

    changed .First.load to .onLoad
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@257 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,11 +1,11 @@
 ### R code from vignette source 'Rnw/zzz.Rnw'
 
 ###################################################
-### code chunk number 1: zzz.Rnw:1-33
+### code chunk number 1: zzz.Rnw:1-35
 ###################################################
 
-.First.lib <- function(libname, pkgname) { #this works for R < 2.14
-#.onLoad <- function(libname, pkgname) {
+#.First.lib <- function(libname, pkgname) { #this works for R < 2.14
+.onLoad <- function(libname, pkgname) {
   #Load the necessary packages
   
 # Removed as now part of namespace  
@@ -19,8 +19,10 @@
   #Read the table of the hypgeom_2F1 function for parameters c(1/3,2/3) and
   #5/3 -- atm this is computed for the values seq(0,10,by=0.01) and 11:100
   #Load the pre-evaluated Hypergeometric function for computing Anscombe residuals
-  surveillance.gvar.hyp <<- scan(file.path(.path.package('surveillance'),'data',"hypGeomSmall.txt"),quiet=TRUE)
-  surveillance.gvar.z <<- - c(0:1000/100, 11:100)
+  data("hypGeomSmall",package=pkgname)
+  
+ # surveillance.gvar.hyp <<- scan(file.path(.path.package('surveillance'),'data',"hypGeomSmall.txt"),quiet=TRUE)
+ # surveillance.gvar.z <<- - c(0:1000/100, 11:100)
 
 # Removed. Now part of namespace
 #  #Load the C code library for the glr stuff  
