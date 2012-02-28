aad5e21c60da237d5598fa063dda0ef8440c8a3e
hoehle (Michael Höhle) <hoehle@math.su.se>
2012-02-28 22:47:55 +0000

    updated a little more of the documentation to produce fewer r cmd check warnings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,18 +1,10 @@
 ### R code from vignette source 'Rnw/zzz.Rnw'
 
 ###################################################
-### code chunk number 1: zzz.Rnw:1-35
+### code chunk number 1: zzz.Rnw:1-21
 ###################################################
 
-#.First.lib <- function(libname, pkgname) { #this works for R < 2.14
 .onLoad <- function(libname, pkgname) {
-  #Load the necessary packages
-  
-# Removed as now part of namespace  
-#  library(spc)
-#  library(maptools)
-#  library(msm)
-
   #Load the CIdata thing
   data(CIdata, package=pkgname)
 
@@ -21,18 +13,12 @@
   #Load the pre-evaluated Hypergeometric function for computing Anscombe residuals
   data("hypGeomSmall",package=pkgname)
   
- # surveillance.gvar.hyp <<- scan(file.path(.path.package('surveillance'),'data',"hypGeomSmall.txt"),quiet=TRUE)
- # surveillance.gvar.z <<- - c(0:1000/100, 11:100)
-
-# Removed. Now part of namespace
-#  #Load the C code library for the glr stuff  
-# library.dynam("surveillance", pkgname, libname)
-  
-  #License limitation for package gpclib
-  cat("\n\tNote: polygon geometry computations related to",
+  #License limitation for package gpclib. Now as part of 
+  #packageStartupMessage
+  packageStartupMessage(paste("\n\tNote: polygon geometry computations related to",
       "\tthe \"epidataCS\" class in surveillance depend on",
       "\tthe package gpclib, which has a restricted licence.\n",
-      sep="\n")
+      sep="\n"),appendLF=FALSE)
 
 }
 
