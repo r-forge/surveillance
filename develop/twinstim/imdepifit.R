5c947e14b1b6b2d3b2407f877411301ccc4c9544
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2013-08-29 22:15:53 +0000

    updated imdepifit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@634 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- develop/twinstim/imdepifit.R
+++ develop/twinstim/imdepifit.R
@@ -1,26 +1,34 @@
 ################################################################################
 ### Author: Sebastian Meyer [sebastian *.* meyer *a*t* ifspm *.* uzh *.* ch]
-### Time-stamp: <[imdepifit.R] by SM Mon 10/12/2012 12:42 (CET)>
+### Time-stamp: <[imdepifit.R] by SM Don 29/08/2013 17:13 (CEST)>
 ### Project: reproduce data(imdepifit)
 ################################################################################
 
 
 ### data(imdepifit) is obtained as
 
-data(imdepi)
-data(imdepifit)
+library("surveillance")
+data("imdepi")
 
 myimdepifit <- twinstim(
-    endemic = ~1 + offset(log(popdensity)) + I(start/365) + 
-        sin(start * 2 * pi/365) + cos(start * 2 * pi/365),
-    epidemic = ~1 + type + agegrp, siaf = siaf.gaussian(1, F.adaptive=TRUE),
+    endemic = addSeason2formula(~ offset(log(popdensity)) + I(start/365-3.5),
+                                S=1, period=365, timevar="start"),
+    epidemic = ~ type + agegrp, siaf = siaf.gaussian(1, F.adaptive=TRUE),
     data = imdepi, subset = !is.na(agegrp),
     control.siaf = list(F=list(adapt=0.25), Deriv=list(nGQ=13)), 
-    optim.args = list(par = c(-20, -0.041, 0.24, 0.35, -17, 0, 0, 0, 4)),
-    # initial values obtained from model with epidemic=~1 and constant siaf
+    optim.args = list(par = c(-20, -0.1, 0.2, 0.3, -12, -1, 0, 0, 3),
+                      control = list(trace=1)),
     model = FALSE, cumCIF = TRUE
 )
 
+## compare with the one stored in the package
+data("imdepifit")
 all.equal(imdepifit, myimdepifit)
 ## only the "runtime" component should be different!
 
+if (FALSE) # store updated fit in data-folder
+{
+    imdepifit <- myimdepifit
+    save(imdepifit, file="~/Projekte/surveillance/pkg/data/imdepifit.RData")
+    tools::resaveRdaFiles("~/Projekte/surveillance/pkg/data/imdepifit.RData")
+}
