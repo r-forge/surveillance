73a995103a649db89c44aa15e786d42e94db88e1
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2013-09-23 14:22:49 +0000

    manuscript: some technical improvements
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@648 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- JSS/setup.R
+++ JSS/setup.R
@@ -1,9 +1,24 @@
+
+## knitr stuff
 opts_chunk$set(echo=FALSE, results='markup', fig.path='plots/R-',
                out.width='.49\\linewidth', fig.width=6, fig.height=6,
                cache=TRUE, cache.path='.cache/')
-render_sweave() # use Sweave environments
-set_header(highlight = '') # do not use the Sweave.sty package
+render_sweave()  # use Sweave environments
+set_header(highlight = '')  # do not use the Sweave.sty package
+
+## R settings
 pdf.options(family = 'Palatino')
-options(prompt="R> ", continue="+  ", width=70, useFancyQuotes=FALSE)   # JSS
-options(digits=3, xtable.booktabs = TRUE)
-set.seed(1613)  # for reproducibility
+options(prompt="R> ", continue="+  ", width=70, useFancyQuotes=FALSE)  # JSS
+options(digits=3, xtable.booktabs=TRUE)
+options(scipen=1)  # so that 1e-4 gets still printed as 0.0001
+
+## Further packages
+library("lattice")
+# Set default plot order for lattice graphics
+local({
+    latticeDefaults <- lattice.getOption("default.args")
+    latticeDefaults$as.table <- TRUE   # go left to right, top to bottom by default
+    lattice.options(default.args = latticeDefaults)
+})
+library("surveillance")
+surveillance.options(gpclib=TRUE)
