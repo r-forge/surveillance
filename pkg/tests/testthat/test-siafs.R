0f1e149d0ad1b6442b6aa8fd58eef3a7c622b326
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2014-02-17 11:01:48 +0000

    more siaf tests also using polyCub:::checkintrfr()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@774 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/testthat/test-siafs.R
+++ pkg/tests/testthat/test-siafs.R
@@ -1,34 +1,78 @@
 context("Spatial interaction functions")
 
-myexpectation <- function (siaf, pars, ...) {
-    checksiafres <- surveillance:::checksiaf(siaf, t(pars), ...)
+
+### test bundle
+
+myexpectation <- function (siaf, intrfr, intrderivr, pargrid, type = 1, ...)
+{
+    ## check analytical intrfr specification against numerical approximation
+    if (!missing(intrfr)) apply(pargrid, 1, function (pars) expect_that(
+        polyCub:::checkintrfr(intrfr, siaf$f, pars, type, center=c(0,0),
+                              rs=c(1,2,5,10,20,50)),
+        not(gives_warning()), label = "polyCub:::checkintrfr()"))
+    
+    ## also check intrfr for deriv
+    if (!missing(intrderivr)) for (paridx in seq_along(intrderivr))
+        apply(pargrid, 1, function (pars) expect_that(
+            polyCub:::checkintrfr(intrderivr[[paridx]],
+                                  function (...) siaf$deriv(...)[,paridx],
+                                  pars, type, center=c(0,0),
+                                  rs=c(1,2,5,10,20,50)),
+            not(gives_warning()),
+            label = paste0("polyCub:::checkintrfr() for deriv[,",paridx,"]")))
+
+    ## check deriv, F, Deriv against numerical approximations
+    checksiafres <- surveillance:::checksiaf(siaf, pargrid, type, ...)
     for (i in which(!sapply(checksiafres, is.null)))
         expect_true(unique(attr(checksiafres[[i]], "all.equal")),
-                    label=paste0(names(checksiafres)[i], " for pars=",
-                    deparse(substitute(pars))))
+                    label=names(checksiafres)[i])
 }
 
+
+### test all pre-defined spatial interaction functions
+
 test_that("Gaussian implementation agrees with numerical approximation",
-          myexpectation(siaf.gaussian(logsd=TRUE, F.adaptive=TRUE),
-                        pars=log(1), tolerance=0.01))
+          myexpectation(siaf.gaussian(F.adaptive=TRUE),
+                        pargrid=t(log(0.5)),
+                        tolerance=0.0005, method="midpoint", dimyx=250))
 
 test_that("Power-law implementation agrees with numerical approximation",
-          myexpectation(siaf.powerlaw(), pars = c(0.5,-0.5), tolerance=0.0005))
+          myexpectation(siaf.powerlaw(),
+                        surveillance:::intrfr.powerlaw,
+                        list(surveillance:::intrfr.powerlaw.dlogsigma,
+                             surveillance:::intrfr.powerlaw.dlogd),
+                        pargrid=cbind(0.5,log(c(0.1,1,2))),
+                        tolerance=0.0005, method="SV", nGQ=13))
 
-test_that("Lagged power-law implementation agrees with numerical results",
-          myexpectation(siaf.powerlawL(), pars=c(-0.5,-0.5), tolerance=0.0005))
+test_that("Lagged power-law implementation agrees with numeric results",
+          if (packageVersion("polyCub") < "0.4-3") { # checkintrfr() too strict
+              myexpectation(siaf.powerlawL(),
+                            pargrid=cbind(-0.5,log(c(0.1,1,2))),
+                            tolerance=0.0005, method="midpoint", dimyx=250)
+          } else {
+              myexpectation(siaf.powerlawL(),
+                            surveillance:::intrfr.powerlawL,
+                            list(surveillance:::intrfr.powerlawL.dlogsigma,
+                                 surveillance:::intrfr.powerlawL.dlogd),
+                            pargrid=cbind(-0.5,log(c(0.1,1,2))),
+                            tolerance=0.0005, method="midpoint", dimyx=250)
+          })
 
-test_that("Student (t) implementation agrees with numerical approximation",
-          myexpectation(siaf.student(), pars=c(0.5,-0.5), tolerance=1e-6))
+test_that("Student implementation agrees with numerical approximation",
+          myexpectation(siaf.student(),
+                        surveillance:::intrfr.student,
+                        list(surveillance:::intrfr.student.dlogsigma,
+                             surveillance:::intrfr.student.dlogd),
+                        pargrid=cbind(0.5,log(c(0.1,1,2))),
+                        tolerance=0.0005, method="SV", nGQ=5))
 
-test_that("Step kernel implementation agrees with numerical approximation", {
-    spatstat::spatstat.options(npixel=350)
-    myexpectation(siaf.step(c(0.1,0.5,1)), pars=-c(0.5,0.1,0.2),
-                  method="midpoint", tolerance=0.0005)
-})
+test_that("Step kernel implementation agrees with numerical approximation",
+          myexpectation(siaf.step(c(0.1,0.5,1)),
+                        pargrid=-t(c(0.5,0.1,0.2)),
+                        tolerance=0.0005, method="midpoint", dimyx=350))
 
 
-### plot the polygon on which F and Deriv are tested:
+## ## plot the polygon on which F and Deriv are tested (to choose parameters)
 ## showsiaf <- function (siaf, pars) {
 ##     data("letterR", package="spatstat", envir=environment())
 ##     poly <- spatstat::shift.owin(letterR, -c(3,2))
