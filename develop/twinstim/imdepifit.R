832b8087358061a9af46a923ddc1f8eb1a7f85ba
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2014-04-08 21:25:29 +0000

    updated imdepifit to match JSS paper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@901 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- develop/twinstim/imdepifit.R
+++ develop/twinstim/imdepifit.R
@@ -1,7 +1,7 @@
 ################################################################################
 ### Author: Sebastian Meyer [sebastian *.* meyer *a*t* ifspm *.* uzh *.* ch]
-### Time-stamp: <[imdepifit.R] by SM Die 08/04/2014 18:23 (CEST)>
-### Project: reproduce data(imdepifit), should be as in the JSS paper
+### Time-stamp: <[imdepifit.R] by SM Die 08/04/2014 23:22 (CEST)>
+### Project: reproduce data(imdepifit) as in the JSS paper (two-stage procedure)
 ################################################################################
 
 
@@ -18,29 +18,16 @@ imdepifit_endemic <- twinstim(
 imdepifit <- update(imdepifit_endemic,
     epidemic = ~ type + agegrp, siaf = siaf.gaussian(),
     control.siaf = list(F=list(adapt=0.25), Deriv=list(nGQ=13)),
-    start = list(epidemic=c("(Intercept)"=-12.5, "typeC"=-1, "siaf.1"=2.8)))
-
-
-myimdepifit <- twinstim(
-    endemic = addSeason2formula(~ offset(log(popdensity)) + I(start/365-3.5),
-                                S=1, period=365, timevar="start"),
-    epidemic = ~ type + agegrp,
-    siaf = siaf.gaussian(nTypes=1, F.adaptive=TRUE),
-    data = imdepi, subset = !is.na(agegrp),
-    control.siaf = list(F=list(adapt=0.25), Deriv=list(nGQ=13)), 
-    optim.args = list(par = c(-20, 0, 0.2, 0.3, -12, -1, 0, 0, 3)),
-    model = FALSE, cumCIF = TRUE
-)
+    start = list(epidemic=c("(Intercept)"=-12.5, "typeC"=-1, "siaf.1"=2.7)))
 
 ## compare with the one stored in the package
-data("imdepifit")
-all.equal(imdepifit, myimdepifit)
+imdepifit_installed <- local({data("imdepifit", envir=environment()); imdepifit})
+all.equal(imdepifit_installed, imdepifit)
 ## only the "runtime" component should be different!
-rbind(imdepifit$runtime, myimdepifit$runtime)
+rbind(imdepifit_installed$runtime, imdepifit$runtime)
 
 if (FALSE) # store updated fit in data-folder
 {
-    imdepifit <- myimdepifit
     save(imdepifit, file="~/Projekte/surveillance/pkg/data/imdepifit.RData")
     tools::resaveRdaFiles("~/Projekte/surveillance/pkg/data/imdepifit.RData")
 }
