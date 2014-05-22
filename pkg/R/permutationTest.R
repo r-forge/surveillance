aab85d421b7cfb51248472617f9a176f48f515a8
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2014-05-22 11:47:29 +0000

    documented Michaela's permutationTest()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@926 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/permutationTest.R
+++ pkg/R/permutationTest.R
@@ -3,23 +3,22 @@
 ### Free software under the terms of the GNU General Public License, version 2,
 ### a copy of which is available at http://www.r-project.org/Licenses/.
 ###
-### Permutation test two compare the means of two samples
+### Permutation test to compare the means of paired samples
 ###
-### Copyright (C) 2010-2014 Michaela Paul and Sebastian Meyer
+### Copyright (C) 2010-2012 Michaela Paul
 ### $Revision$
 ### $Date$
 ################################################################################
 
-permutationTest <- function(score1, score2, nPermutation=9999,
-                            paired=TRUE, plot=FALSE)
+permutationTest <- function(score1, score2, nPermutation=9999, plot=FALSE)
 {
-    if (!isTRUE(paired)) stop("independent samples are not implemented yet")
+    stopifnot((nTime <- length(score1)) == length(score2),
+              !is.na(score1), !is.na(score2))
     
     meanScore1 <- mean(score1)
     meanScore2 <- mean(score2)
     diffObserved <- meanScore1 - meanScore2
     
-    nTime <- length(score1)
     diffMean <- replicate(nPermutation, {
         sel <- rbinom(nTime, size=1, prob=0.5)
         g1 <- (sum(score1[sel==0]) + sum(score2[sel==1]))/nTime
@@ -32,13 +31,13 @@ permutationTest <- function(score1, score2, nPermutation=9999,
         abline(v=diffObserved,col=4)
     }
     
-    pVal <- (1+sum(abs(diffMean)>=abs(diffObserved)))/(nPermutation+1)
+    pVal <- (1+sum(abs(diffMean)>=abs(diffObserved))) / (nPermutation+1)
 
-    pTtest <- t.test(score1,score2,paired=paired)$p.value
+    pTtest <- t.test(score1, score2, paired=TRUE)$p.value
     
     cat("mean difference=", diffObserved,
         "\tp(permutation) =", pVal,
         "\tp(paired t-test) =", pTtest, "\n")
     
-    list(diffObs=diffObserved, pVal.permut=pVal,pVal.t=pTtest)
+    list(diffObs=diffObserved, pVal.permut=pVal, pVal.t=pTtest)
 }
