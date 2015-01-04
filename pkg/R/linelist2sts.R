4e67d5fe7317af789e21ec859ad28924c0d34623
hoehle (Michael Höhle) <hoehle@math.su.se>
2015-01-04 22:45:58 +0000

    updated DESCRIPTION file to use new INLA add-on repository. Also contains improved linelist2sts function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/linelist2sts.R
+++ pkg/R/linelist2sts.R
@@ -2,7 +2,6 @@
 # Takes a data frame with dates of individual
 # cases and create an aggregated sts time series object for these
 # data with aggregation occuring at the desired scale.
-# Note: This function is experimental
 #
 # Parameters:
 #  linelist - a data frame containing individual case information, one per line
@@ -12,28 +11,30 @@
 #       seq.Date -- see \link{seq.Date} for further details.
 #
 # Author: Michael Hoehle
-# Date LaMo: 06 Feb 2012
+# Date LaMo: 04 Jan 2014
 ######################################################################
 
-linelist2sts <- function(linelist,dateCol,aggregate.by="1 week",dRange=NULL,
+linelist2sts <- function(linelist,dateCol,aggregate.by=c("1 day", "1 week", "7 day", "1 week", "1 month", "3 month", "1 year"),dRange=NULL,
                       startYearFormat=switch(aggregate.by,"1 day"="%V","7 day"="%V","1 week"="%V","1 month"="%Y","3 month"="%Y"),
                       startEpochFormat=switch(aggregate.by,"1 day"="%j","7 day"="%V","1 week"="%V","1 month"="%m","3 month"="%Q")
                       ) {
- #by <- match.arg(by,c("1 day","1 week","1 month","1 year")
+
+  ##Check aggregate.by argument
+  aggregate.by <- match.arg(aggregate.by, c("1 day", "1 week", "7 day", "1 week", "1 month", "3 month", "1 year"))
+  epochInPeriodStr <- switch(aggregate.by, "1 day"="1","1 week"="%u", "1 month"="%d","1 year"="%j")
+  
+  #If no dRange let it be the range of the dateCol
   if (is.null(dRange)) {
     dRange <- range(linelist[,dateCol],na.rm=TRUE)
   }
-
-  ## #Make sure that if weeks we span the entire data set.
-  ## if ((aggregate.by=="1 week" | aggregate.by == "7 day") & is.null(dRange)) {
-  ##   #Adjust first date to a monday and the last to be a sunday
-  ##   weekDay <- as.numeric(format(dRange, "%w"))
-  ##   dRange[1] <- dRange[1] - ifelse( weekDay[1] == 0, 6, (weekDay[1]-1))
-  ##   dRange[2] <- dRange[2] + 7
-  ## }
-
+  if (aggregate.by != "1 day") {
+    ##Move dates back to first of each epoch unit
+    dRange <-  dRange - as.numeric(format(dRange,epochInPeriodStr)) + 1
+  }
+  
   #Add exactly one time step to dRange to ensure that cut
-  #contains the respective level
+  #contains the last level as well. We use 'seq' to ensure
+  #that even weeks/days with no data are present in the factor.
   maxDate <- seq(max(dRange),length.out=2,by=aggregate.by)[-1]
   dates <- seq(min(dRange), maxDate, by=aggregate.by)
 
@@ -47,8 +48,8 @@ linelist2sts <- function(linelist,dateCol,aggregate.by="1 week",dRange=NULL,
   #Translate "by" to freq string
   freq <- switch(aggregate.by,"1 day"=365,"7 day"=52,"1 week"=52,"1 month"=12,"3 month"=4)
 
-  startYear <- as.numeric(formatDate(min(dates),startYearFormat))
-  startEpoch <- as.numeric(formatDate(min(dates),startEpochFormat))
+  startYear <- as.numeric(format(min(dates),startYearFormat))
+  startEpoch <- as.numeric(format(min(dates),startEpochFormat))
                   
   observed <- matrix(observed,ncol=1)
 
