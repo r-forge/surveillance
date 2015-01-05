a9126fb15702b2c1229113185d27887735667ad5
hoehle (Michael Höhle) <hoehle@math.su.se>
2015-01-05 10:53:09 +0000

    updated documentation for nowcast and linelist2sts
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/linelist2sts.R
+++ pkg/R/linelist2sts.R
@@ -15,13 +15,13 @@
 ######################################################################
 
 linelist2sts <- function(linelist,dateCol,aggregate.by=c("1 day", "1 week", "7 day", "1 week", "1 month", "3 month", "1 year"),dRange=NULL,
+                      epochInPeriodStr=switch(aggregate.by, "1 day"="1","1 week"="%u", "1 month"="%d","3 month"="%q","1 year"="%j"),
                       startYearFormat=switch(aggregate.by,"1 day"="%V","7 day"="%V","1 week"="%V","1 month"="%Y","3 month"="%Y","1 year"="%Y"),
                       startEpochFormat=switch(aggregate.by,"1 day"="%j","7 day"="%V","1 week"="%V","1 month"="%m","3 month"="%Q","1 year"="1")
                       ) {
 
   ##Check aggregate.by argument
   aggregate.by <- match.arg(aggregate.by, c("1 day", "1 week", "7 day", "1 week", "1 month", "3 month", "1 year"))
-  epochInPeriodStr <- switch(aggregate.by, "1 day"="1","1 week"="%u", "1 month"="%d","3 month"="%q","1 year"="%j")
   
   #If no dRange let it be the range of the dateCol
   if (is.null(dRange)) {
