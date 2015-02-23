5f255d36c827a4750a0507e12c9a8ba51e8e916c
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2015-02-23 17:02:38 +0000

    use new plapply() in epitest()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/twinstim_epitest.R
+++ pkg/R/twinstim_epitest.R
@@ -10,14 +10,11 @@
 ### $Date$
 ################################################################################
 
-epitest <- function (model, data, B = 199, seed = NULL,
-                     cores = 1, verbose = TRUE)
+epitest <- function (model, data, B = 199, seed = NULL, verbose = TRUE, ...)
 {
     stopifnot(inherits(model, "twinstim"), inherits(data, "epidataCS"),
-              model$converged, isScalar(B), B >= 1, isScalar(cores), cores >= 1,
-              length(verbose) == 1L)
+              model$converged, isScalar(B), B >= 1)
     B <- as.integer(B)
-    cores <- as.integer(cores)
     if (model$npars["q"] == 0L) {
         stop("no epidemic component in 'model'")
     }
@@ -42,58 +39,30 @@ epitest <- function (model, data, B = 199, seed = NULL,
     LRT <- lrt(m0 = m0, m1 = model)
     STATISTIC <- structure(LRT["D"], l0 = LRT[["l0"]], l1 = LRT[["l1"]])
     
-    ## progress bar
-    increment_pb <- if (is.character(verbose)) { # progress symbol
-        on.exit(cat("\n"))
-        function (...) cat(verbose)
-    } else {
-        verbose <- as.integer(verbose)
-        if (verbose <= 0L) {        # no output
-            function (...) {}
-        } else if (verbose == 1L) { # progress bar or dots
-            if (cores == 1L && interactive()) {
-                pb <- txtProgressBar(min = 0, max = B, initial = 0, style = 3)
-                on.exit(close(pb))
-                function (...) setTxtProgressBar(pb, pb$getVal() + 1L)
-            } else {
-                on.exit(cat("\n"))
-                function (...) cat(".")
+    ## 'verbose' expression to print test statistics
+    if (is.numeric(verbose) && verbose >= 2L) {
+        cat("Endemic/Epidemic log-likelihoods and LRT statistic:\n",
+            paste0(names(LRT)[1:3], " = ", sprintf("%4.1f", LRT[1:3]),
+                   collapse = " | "),
+            "\n\nResults from B=", B, " permutations of the event times:\n",
+            sep = "")
+        verbose <- quote({
+            lrt <- lrt(m0, m1)
+            cat(paste0(names(lrt)[1:3], " = ", sprintf("%4.1f", lrt[1:3]),
+                       collapse = " | "))
+            if (!lrt["converged"]) {
+                msg <- c(m0 = m0$converged, m1 = m1$converged)
+                msg <- msg[msg != "TRUE"]
+                cat(" | WARNING (", paste0(names(msg), collapse = " and "),
+                    "): ", paste0(unique(msg), collapse = " and "), sep = "")
             }
-        } else if (verbose >= 2L) { # print test statistics
-            cat("Endemic/Epidemic log-likelihoods and LRT statistic:\n",
-                paste0(names(LRT)[1:3], " = ", sprintf("%4.1f", LRT[1:3]),
-                       collapse = " | "),
-                "\n\nResults from B=", B, " permutations of the event times:\n",
-                sep = "")
-            function (m0, m1) {
-                lrt <- lrt(m0, m1)
-                cat(paste0(names(lrt)[1:3], " = ", sprintf("%4.1f", lrt[1:3]),
-                           collapse = " | "))
-                if (!lrt["converged"]) {
-                    msg <- c(m0 = m0$converged, m1 = m1$converged)
-                    msg <- msg[msg != "TRUE"]
-                    cat(" | WARNING (", paste0(names(msg), collapse = " and "),
-                        "): ", paste0(unique(msg), collapse = " and "), sep = "")
-                }
-                cat("\n")
-            }
-        }
-    }
-    
-    ## set random seed for reproducibility
-    if (!is.null(seed)) {
-        if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
-            set.seed(NULL)
-        .orig.seed <- get(".Random.seed", envir = .GlobalEnv)
-        on.exit(assign(".Random.seed", .orig.seed, envir = .GlobalEnv),
-                add = TRUE)
-        set.seed(seed = seed, kind = if (cores > 1L) "L'Ecuyer-CMRG")
-        ##parallel::mc.reset.stream()  # redundant, mclapply() takes care itself
+            cat("\n")
+        })
     }
     
     ## define the function to be replicated B times:
     ## permute data, update epidemic model, compute endemic-only model
-    permfits1 <- function (...) {
+    permfits1 <- function (...) { # needs 'data' and 'model'
         .permdata <- permute.epidataCS(data, what = "time", keep = time <= t0)
         m1 <- update.twinstim(model, data = .permdata,
                               model = FALSE, cumCIF = FALSE,
@@ -101,18 +70,12 @@ epitest <- function (model, data, B = 199, seed = NULL,
                               optim.args = list(control = list(trace = 0)))
         m0 <- update.twinstim(m1, epidemic = ~0, siaf = NULL, tiaf = NULL,
                               control.siaf = NULL)
-        increment_pb(m0, m1)
         list(m0 = m0, m1 = m1)
     }
 
     ## rock'n'roll (the computationally intensive part)
-    permfits <- if (cores == 1L) {
-        lapply(X = integer(B), FUN = permfits1)
-    } else {
-        parallel::mclapply(X = integer(B), FUN = permfits1,
-                           mc.preschedule = TRUE, mc.set.seed = TRUE,
-                           mc.silent = FALSE, mc.cores = cores)
-    }
+    permfits <- plapply(X = integer(B), FUN = permfits1,
+                        .seed = seed, .verbose = verbose, ...)
     
     ## compute the test statistic
     permstats <- as.data.frame(t(vapply(
