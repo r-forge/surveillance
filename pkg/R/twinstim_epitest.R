0f53d2a01bfe968133d5fc048b9086537a8cc644
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2015-03-24 15:06:14 +0000

    epitest(..., verbose = 3) for tracing loglik maximization in each permutation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1287 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/twinstim_epitest.R
+++ pkg/R/twinstim_epitest.R
@@ -52,35 +52,41 @@ epitest <- function (model, data, B = 199, eps.s = NULL, eps.t = NULL,
     STATISTIC_D <- structure(LRT["D"], l0 = LRT[["l0"]], l1 = LRT[["l1"]])
     STATISTIC_R0 <- c("simpleR0" = simpleR0(model, eps.s = eps.s, eps.t = eps.t))
     
-    ## 'verbose' expression to print test statistics
-    if (is.numeric(verbose) && verbose >= 2L) {
-        stats2string <- function (lrt, simpleR0)
-            paste0(c(names(lrt)[1:3], "simpleR0"), " = ",
-                   sprintf(paste0("%4.", c(0,0,1,2), "f"), c(lrt[1:3], simpleR0)),
-                   collapse = " | ")
-        cat("Endemic/Epidemic log-likelihoods, LRT statistic, and simple R0:\n",
-            stats2string(LRT, STATISTIC_R0), "\n",
-            "\nResults from B=", B, " permutations of the event times:\n",
-            ## will actually not be printed if parallelized using clusters ...
-            sep = "")
-        verbose <- substitute({
-            cat(STATS2STRING)
-            if (!lrt["converged"]) {
-                msg <- c(m0 = m0$converged, m1 = m1$converged)
-                msg <- msg[msg != "TRUE"]
-                cat(" | WARNING (", paste0(names(msg), collapse = " and "),
-                    "): ", paste0(unique(msg), collapse = " and "), sep = "")
-            }
-            cat("\n")
-        }, list(STATS2STRING = body(stats2string)))
-    }
+    ## interpret 'verbose' level
+    .verbose <- if (is.numeric(verbose)) {
+        if (verbose >= 2) {
+            ## create '.verbose' expression to print test statistics
+            stats2string <- function (lrt, simpleR0)
+                paste0(c(names(lrt)[1:3], "simpleR0"), " = ",
+                       sprintf(paste0("%4.", c(0,0,1,2), "f"), c(lrt[1:3], simpleR0)),
+                       collapse = " | ")
+            cat("Endemic/Epidemic log-likelihoods, LRT statistic, and simple R0:\n",
+                stats2string(LRT, STATISTIC_R0), "\n",
+                "\nResults from B=", B, " permutations of the event times:\n",
+                ## will actually not be printed if parallelized using clusters ...
+                sep = "")
+            substitute({
+                cat(STATS2STRING)
+                if (!lrt["converged"]) {
+                    msg <- c(m0 = m0$converged, m1 = m1$converged)
+                    msg <- msg[msg != "TRUE"]
+                    cat(" | WARNING (", paste0(names(msg), collapse = " and "),
+                        "): ", paste0(unique(msg), collapse = " and "), sep = "")
+                }
+                cat("\n")
+            }, list(STATS2STRING = body(stats2string)))
+        } else {
+            verbose <- verbose == 1
+        }
+    } else verbose
 
     ## if siafpars are fixed, determine siafInt for use in all permutations
     siafInt <- NULL
     siafpars <- coeflist(model)$siaf
     if (length(siafpars) > 0L && all(names(siafpars) %in% fixed) &&
         is.null(siafInt <- environment(model)$siafInt)) {
-        cat("pre-evaluating 'siaf' integrals with fixed parameters ...\n")
+        if (!identical(FALSE, verbose))
+            cat("pre-evaluating 'siaf' integrals with fixed parameters ...\n")
         setup <- update.twinstim(model, data = data, optim.args = NULL, verbose = FALSE)
         assign("siafpars", siafpars, envir = environment(setup))
         siafInt <- with(environment(setup), do.call("..siafInt", .siafInt.args))
@@ -98,7 +104,8 @@ epitest <- function (model, data, B = 199, eps.s = NULL, eps.t = NULL,
                               control.siaf = list(siafInt = .siafInt),
                               model = FALSE, cumCIF = FALSE,
                               cores = 1, verbose = FALSE,
-                              optim.args = list(fixed = fixed, control = list(trace = 0)))
+                              optim.args = list(fixed = fixed,
+                                  control = list(trace = is.numeric(verbose) && verbose >= 3)))
         ## sink()
         m0 <- update.twinstim(m1, epidemic = ~0, siaf = NULL, tiaf = NULL,
                               control.siaf = NULL)
@@ -110,7 +117,7 @@ epitest <- function (model, data, B = 199, eps.s = NULL, eps.t = NULL,
 
     ## rock'n'roll (the computationally intensive part)
     permfits <- plapply(X = integer(B), FUN = permfits1,
-                        .verbose = verbose, ...)
+                        .verbose = .verbose, ...)
     
     ## extract the statistics
     permstats <- as.data.frame(t(vapply(
