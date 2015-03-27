ab4993abc5b6e8c2121f9d270b411d2222e7bf3c
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2015-03-27 13:19:29 +0000

    check arguments and tidy code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1299 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/addSeason2formula.R
+++ pkg/R/addSeason2formula.R
@@ -10,33 +10,42 @@
 ### $Date$
 ################################################################################
 
-## basically, 'sin(2*pi * t/period) + cos(2*pi * t/period)' is added to 'f'
+## for S = 1, 'sin(2*pi * t/period) + cos(2*pi * t/period)' is added to 'f'
 addSeason2formula <- function (
     f = ~1,       # formula to enhance
     S = 1,        # number of sine/cosine pairs
     period = 52,  # periodicity of the sinusoidal wave
     timevar = "t" # name of the time variable
 ){
+    ## check arguments
+    stopifnot(inherits(f, "formula"),
+              is.vector(S, mode = "numeric"), S >= 0,
+              isScalar(period))
+    
     ## return unchanged formula if S = 0
     if (max(S) == 0)
         return(f)
-  
-    f <- paste(deparse(f), collapse = "")
-    ## create formula
-    if (length(S) == 1 && S > 0) {
-        for (i in seq_len(S)){
-            f <- paste0(f,
-                        " + sin(",2*i,"*pi*",timevar,"/",period,")",
-                        " + cos(",2*i,"*pi*",timevar,"/",period,")")
+
+    ## character representation of old formula
+    ftext <- paste0(deparse(f), collapse = "")
+    
+    ## add sine-cosine terms
+    if (length(S) == 1L) {
+        for (i in seq_len(S)) {
+            ftext <- paste0(ftext,
+                " + sin(", 2*i, "*pi*", timevar, "/", period, ")",
+                " + cos(", 2*i, "*pi*", timevar, "/", period, ")")
         }
     } else {
-        for (i in seq_len(max(S))){
-            which <- paste(i <= S,collapse=",")
-            f <- paste0(f,
-                        " + fe( sin(",2*i,"*pi*",timevar,"/",period,"), which=c(",which,"))",
-                        " + fe( cos(",2*i,"*pi*",timevar,"/",period,"), which=c(",which,"))")
+        ## unit-specific seasonality for hhh4() via the special fe() function
+        for (i in seq_len(max(S))) {
+            which <- paste0(i <= S, collapse = ",")
+            ftext <- paste0(ftext,
+                " + fe(sin(",2*i,"*pi*",timevar,"/",period,"), which=c(",which,"))",
+                " + fe(cos(",2*i,"*pi*",timevar,"/",period,"), which=c(",which,"))")
         }
     }
-    
-    as.formula(f, env = .GlobalEnv)
+
+    ## convert back to a formula
+    as.formula(ftext, env = .GlobalEnv)
 }
