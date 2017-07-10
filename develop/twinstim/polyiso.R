52401968bd099b66efef445e394dc9c693a2df9e
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2017-07-10 07:28:08 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1928 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- develop/twinstim/polyiso.R
+++ develop/twinstim/polyiso.R
@@ -1,20 +1,21 @@
 ## debug siaf_polyCub1_iso on Solaris for d == 2.0
-setwd("../../pkg/src/")
+#setwd("../../pkg/src/")
+#setwd("surveillance/pkg/src")
 
 xypoly <- spatstat::letterR$bdry[[1]]
-intrfr_code <- 10  # intrfr.powerlaw
-intrfr_code <- 30  # intrfr.powerlawL
+intrfr_code <- 10  # 30
 pars <- c(1, log(2))
 
 try(dyn.unload("twinstim_siaf_polyCub_iso.so"), silent = TRUE)
 system2("R", "CMD SHLIB --clean twinstim_siaf_polyCub_iso.c",
-        env=paste0("PKG_CPPFLAGS=-I'", system.file("include", package="polyCub"), "'"))
+        env = paste0("PKG_CPPFLAGS=-I'",
+            system.file("include", package="polyCub"),
+        "'"))
 dyn.load("twinstim_siaf_polyCub_iso.so")
 requireNamespace("polyCub")
 
-res <- .C("C_siaf_polyCub1_iso",
-          as.double(xypoly$x), as.double(xypoly$y), as.integer(length(xypoly$x)),
-          as.integer(intrfr_code), as.double(pars),
-          100L, .Machine$double.eps^0.25, .Machine$double.eps^0.25, 0L,
-          value = double(1L), abserr = double(1L), neval = integer(1L))
+res <- .C("C_siaf_polyCub1_iso", as.double(xypoly$x), as.double(xypoly$y),
+          as.integer(length(xypoly$x)), as.integer(intrfr_code),
+          as.double(pars), 100L, 0.0001, 0.0001, 1L,
+          value=double(1L), abserr=double(1L), neval=integer(1L))
 print(unlist(tail(res, 3)))
