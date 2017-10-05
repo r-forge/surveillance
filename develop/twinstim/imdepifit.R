f0db896aecc080560a264ad5f64bc863ebf03aa5
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2017-10-05 15:23:23 +0000

    update imdepifit with new iso-implementation of siaf.gaussian()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@1971 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- develop/twinstim/imdepifit.R
+++ develop/twinstim/imdepifit.R
@@ -1,27 +1,24 @@
 ################################################################################
 ### Author: Sebastian Meyer [seb *.* meyer *a*t* fau *.* de]
-### Time-stamp: <[imdepifit.R] 2017-10-05 16:27 (CEST) by SM>
+### Time-stamp: <[imdepifit.R] 2017-10-05 17:17 (CEST) by SM>
 ### Project: reproduce data(imdepifit)
 ################################################################################
 
-
-### data(imdepifit) is obtained as
-
 library("surveillance")
-data("imdepi")
+data("imdepi", "imdepifit")
 
 myimdepifit <- twinstim(
     endemic = addSeason2formula(~ offset(log(popdensity)) + I(start/365-3.5),
-                                S=1, period=365, timevar="start"),
-    epidemic = ~ type + agegrp, siaf = siaf.gaussian(F.adaptive=TRUE),
+                                S = 1, period = 365, timevar = "start"),
+    epidemic = ~ type + agegrp, siaf = siaf.gaussian(),
     data = imdepi, subset = !is.na(agegrp),
-    control.siaf = list(F=list(adapt=0.25), Deriv=list(nGQ=13)),
-    optim.args = list(par = c(-20, 0, 0.2, 0.3, -12, -1, 0, 0, 3)),
+    optim.args = list(control = list(reltol = sqrt(.Machine$double.eps))),
+                      ## par = c(-20, 0, 0.2, 0.3, -12, -1, 0, 0, 3)),
+                      ## CAVE: start values affect estimates (tolerance ~0.002)
     model = FALSE, cumCIF = FALSE
 )
 
 ## compare with the one stored in the package
-data("imdepifit")
 all.equal(imdepifit, myimdepifit)
 ## only the "runtime" component should be different!
 rbind(imdepifit$runtime, myimdepifit$runtime)
