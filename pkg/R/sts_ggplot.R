188d01182c435dbdeb196ee69e2d435cc1d74788
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2018-01-19 16:20:53 +0000

    provide autoplot.sts()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2059 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/sts_ggplot.R
@@ -0,0 +1,35 @@
+################################################################################
+### Plot a surveillance time series ("sts") object using ggplot2
+###
+### Copyright (C) 2018 Sebastian Meyer
+###
+### This file is part of the R package "surveillance",
+### free software under the terms of the GNU General Public License, version 2,
+### a copy of which is available at http://www.r-project.org/Licenses/.
+################################################################################
+
+autoplot.sts <- function (object, units = NULL, as.one = FALSE,
+                          scales = "fixed", ...)
+{
+    stopifnot(is(object, "sts"))
+    data <- tidy.sts(object)
+
+    ## select subset of units to plot
+    if (!is.null(units)) {
+        ## ensure that 'units' are labels, not indices
+        units <- unname(setNames(nm = levels(data$unit))[units])
+        data <- subset(data, unit %in% units)
+    }
+
+    p <- ggplot2::ggplot(
+        data = data,
+        mapping = ggplot2::aes(x = date, y = observed, group = unit)
+    )
+    if (as.one) {
+        p <- p + ggplot2::geom_line(aes(colour = unit))
+    } else {
+        p <- p + ggplot2::geom_bar(stat = "identity") +
+            ggplot2::facet_wrap(~unit, scales = scales, drop = TRUE)
+    }
+    p + ggplot2::labs(x = "Time", y = "No. infected")
+}
