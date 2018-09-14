3971a544ac50871a5c862fa4ffb6f85dd0bfd67e
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2018-09-14 10:16:18 +0000

    apply(x, 1, sum) -> rowSums(x)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2220 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sts.R
+++ pkg/R/sts.R
@@ -227,12 +227,12 @@ setMethod("aggregate", signature(x="sts"), function(x,by="time",nfreq="all",...)
   #Aggregate units
   if (by == "unit") {
     #Aggregate units
-    x@observed <- as.matrix(apply(x@observed, MARGIN=1, sum))
-    x@state <- as.matrix(apply(x@state, MARGIN=1, sum))>0
-    x@alarm <- as.matrix(apply(x@alarm, MARGIN=1, sum))>0 # contrary to counting for by="time"!
+    x@observed <- as.matrix(rowSums(x@observed))
+    x@state <- as.matrix(rowSums(x@state))>0
+    x@alarm <- as.matrix(rowSums(x@alarm))>0 # contrary to counting for by="time"!
     #There is no clever way to aggregate the upperbounds
     x@upperbound <- matrix(NA_real_,ncol=ncol(x@alarm),nrow=nrow(x@alarm))
-    x@populationFrac <- as.matrix(apply(x@populationFrac, MARGIN=1, sum))#>0
+    x@populationFrac <- as.matrix(rowSums(x@populationFrac))
     x@neighbourhood <- matrix(NA, 1, 1) # consistent with default for new("sts")
     ## we have lost colnames
     colnames(x@observed) <- "overall"
