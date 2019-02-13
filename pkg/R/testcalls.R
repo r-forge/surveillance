085af0a5d5885cbbfefbc11a1a43ce68db02965f
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2019-02-13 15:05:26 +0000

    remove deprecated makePlot()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/testcalls.R
+++ pkg/R/testcalls.R
@@ -22,18 +22,3 @@ test <- function(data = c("k1", "m5"), range = 157:339){
   cat("\n\nSummary:\n")
   print(compMatrix.writeTable(sum))
 }
-
-
-###################################################
-### chunk number 3:
-###################################################
-
-makePlot <- function(outputpath, data = "k1", method = "rki1", name, disease, range = 157:339){
-  .Deprecated(package = "surveillance")
-        disProgObj <- readData(data,week53to52=TRUE)
-        disProgObj <- enlargeData(disProgObj)
-        res <- algo.call(disProgObj, control = list(list(funcName = method, range = range)))
-        pdf(paste(outputpath, data, "_", method, "_plot.pdf", sep=""), width = 10)
-                plot(res[[1]],name,disease)
-        dev.off()
-}
