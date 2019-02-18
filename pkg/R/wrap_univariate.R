6c6bacefe8be3ee4e287fd3f9f8c9a07954d0fb8
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2019-02-18 22:15:21 +0000

    wrap.algo(): make use of [,sts-method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2338 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/wrap_univariate.R
+++ pkg/R/wrap_univariate.R
@@ -43,27 +43,11 @@ wrap.algo <- function(sts, algo, control,
   #Control object needs only to be set once
   sts@control <- survRes.k$control
 
-  #Reduce sts object to only those obervations in range
-  sts@observed <- sts@observed[control$range,,drop=FALSE]
-  sts@state <- sts@state[control$range,,drop=FALSE]
-  sts@populationFrac <- sts@populationFrac[control$range,,drop=FALSE]
-  sts@alarm <- sts@alarm[control$range,,drop=FALSE]
-  sts@upperbound <- sts@upperbound[control$range,,drop=FALSE]
-
   #Set correct theta0t matrix for all
   sts@control$theta0t <- control$theta0t
 
-  #Fix the corresponding start entry
-  start <- sts@start
-  new.sampleNo <- start[2] + min(control$range) - 1
-  start.year <- start[1] + (new.sampleNo - 1) %/% sts@freq
-  start.sampleNo <- (new.sampleNo - 1) %% sts@freq + 1
-  sts@start <- c(start.year,start.sampleNo)
-  sts@epoch <- sts@epoch[control$range]
-  sts@epochAsDate <- sts@epochAsDate
-
-  #Ensure dimnames in the new object
-  sts <- fix.dimnames(sts)
+  #Reduce sts object to only those obervations in range
+  sts <- sts[control$range, ]
 
   return(sts)
 }
