1f580d89241707fcf60cd8b640c4d1ec743c726a
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2019-07-03 12:48:35 +0000

    seems to work now (checked with R-devel on win-builder)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2433 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/testthat/test-plapply.R
+++ pkg/tests/testthat/test-plapply.R
@@ -1,8 +1,6 @@
 context("plapply()")
 
 test_that("plapply() results are reproducible", {
-    if (.Platform$OS.type == "windows")
-        skip("causes R CMD check to hang on Windows (but works interactively)")
     res1 <- plapply(c(1, 1), rnorm, .parallel = 2, .seed = 1, .verbose = FALSE)
     res2 <- plapply(c(1, 1), rnorm, .parallel = 2, .seed = 1, .verbose = FALSE)
     expect_identical(res1, res2)
