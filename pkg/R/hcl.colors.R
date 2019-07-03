ab3a89b214f32f0afcb07df7f998d762f3975e84
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2019-07-03 12:23:01 +0000

    resort to Heat 2 palette from new grDevices::hcl.colors() in R >= 3.6.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2432 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hcl.colors.R
+++ pkg/R/hcl.colors.R
@@ -1,7 +1,7 @@
 ################################################################################
 ### Generate a color palette via the colorspace package
 ###
-### Copyright (C) 2007 Michael Hoehle, 2012-2014,2017 Sebastian Meyer
+### Copyright (C) 2007 Michael Hoehle, 2012-2014,2017,2019 Sebastian Meyer
 ###
 ### This file is part of the R package "surveillance",
 ### free software under the terms of the GNU General Public License, version 2,
@@ -15,8 +15,14 @@ hcl.colors <- function (ncolors=100, use.color=TRUE)
         colorspace::heat_hcl(ncolors, h=c(0,120),
                              c=if (use.color) c(90,30) else c(0,0),
                              l=c(50,90), power=c(0.75, 1.2))
+    } else if (use.color) {
+        if (getRversion() >= "3.6.0") {
+            grDevices::hcl.colors(n = ncolors, palette = "Heat 2")
+        } else {
+            heat.colors(ncolors)
+        }
     } else {
-        if (use.color) heat.colors(ncolors) else grey.colors(ncolors)
+        grey.colors(ncolors)
     }
     return(rev(GYR))
 }
