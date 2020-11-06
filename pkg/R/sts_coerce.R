d9f394b5350b492eb0b43278a91c58563bd8136e
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2020-11-06 10:54:25 +0000

    document possible NA dates and remove message
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2582 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sts_coerce.R
+++ pkg/R/sts_coerce.R
@@ -1,7 +1,7 @@
 ################################################################################
 ### Conversion between "ts" and "sts", and from "sts" to "data.frame"
 ###
-### Copyright (C) 2014 Michael Hoehle, 2015-2017,2019 Sebastian Meyer
+### Copyright (C) 2014 Michael Hoehle, 2015-2017,2019-2020 Sebastian Meyer
 ###
 ### This file is part of the R package "surveillance",
 ### free software under the terms of the GNU General Public License, version 2,
@@ -112,7 +112,7 @@ tidy.sts <- function (x, ...)
     stswide$epochInYear <- epochInYear(x)
     stswide$date <- tryCatch(
         epoch(x, as.Date = TRUE),  # only works for particular values of x@freq
-        error = function (e) {message("Note: ", e$message); as.Date(NA)}
+        error = function (e) as.Date(NA)
     )
     if ((nUnit <- ncol(x)) == 1L) {
         stslong <- data.frame(stswide, "unit" = factor(unitNames),
