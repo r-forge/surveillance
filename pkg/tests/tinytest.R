d0c5bd0e5de41072442af6baa61410123b9b4a86
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2020-11-20 15:55:21 +0000

    my tinytest improvements have been integrated in the current development version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@2594 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/tinytest.R
+++ pkg/tests/tinytest.R
@@ -1,8 +1,9 @@
 ### lazy (!) transition from "testthat" to "tinytest"
 ### Copyright (C) 2020 Sebastian Meyer
 
-if (!requireNamespace("tinytest", quietly = TRUE)) {
-    message("this test suite requires package 'tinytest'")
+if (!requireNamespace("tinytest", quietly = TRUE)
+    || packageVersion("tinytest") < "1.2.3.6") {
+    message("this test suite requires package 'tinytest' (> 1.2.3)")
     q("no")
 }
 
@@ -20,27 +21,6 @@ expect_is <- function (current, class, info = NA_character_) {
     expect_true(inherits(current, class), info = info)
 }
 
-## variant of tinytest::test_package() for non-installed tests;
 ## we use verbose = 1 to print_status() only after each test file,
 ## not after each expression (verbose = 2)
-.test_package <- function (pkgname, testdir, at_home = FALSE,
-                           verbose = 1, ...)
-{
-    library(pkgname, character.only = TRUE)
-
-    ## run test files and collect results
-    out <- tinytest::run_test_dir(testdir, at_home = at_home,
-                                  verbose = verbose, ...)
-
-    ## throw an error if any test fails
-    i_fail <- vapply(out, function (x) !is.na(x) && !x, TRUE)
-    if (any(i_fail) && !interactive()) { # print all failing tests and stop
-        writeLines(vapply(out[i_fail], format, "", type="long"))
-        ## R CMD check will show the last few lines in the log
-        stop(sum(i_fail), " out of ", length(out), " tests failed", call.=FALSE)
-    }
-
-    out
-}
-
-.test_package("surveillance", testdir = "testthat")
+tinytest::test_package("surveillance", testdir = "testthat", verbose = 1)
