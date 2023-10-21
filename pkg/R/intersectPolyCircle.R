f2b0eb0c1c350d6ccabd44f55fb7dd4443605535
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2023-10-21 09:23:21 +0000

    defunctify useGEOS=TRUE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@3014 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/intersectPolyCircle.R
+++ pkg/R/intersectPolyCircle.R
@@ -15,11 +15,7 @@ intersectPolyCircle.gpc.poly <- function (object, center, radius,
 {
     ## gpcWarning()
     if (useGEOS) {
-        .Deprecated(msg = "argument 'useGEOS' will soon be defunct")
-        loadNamespace("rgeos")  # coerce gpc.poly to SpatialPolygons
-        res <- intersectPolyCircle.SpatialPolygons(as(object, "SpatialPolygons"),
-                                               center, radius, npoly)
-        as(res, "gpc.poly")  # also defined in rgeos
+        .Defunct(msg = "'useGEOS = TRUE' is defunct")
     } else {
         gpclibCheck()
         circle <- discpoly(center, radius, npoly = npoly, class = "gpc.poly")
