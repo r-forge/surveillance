ad69b6090c6c88b0bc952c0712e126dfb9e70da1
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2024-11-03 10:31:00 +0000

    disable colorized test tracing for a cleaner log file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surveillance@3152 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/tinytest.R
+++ pkg/tests/tinytest.R
@@ -16,6 +16,8 @@ test_that <- function (desc, code) {
 ## show warnings as they appear
 options(warn = 1)
 
-## we use verbose = 1 to print_status() only after each test file,
-## not after each expression (verbose = 2)
-tinytest::test_package("surveillance", testdir = "testthat", verbose = 1)
+## use verbose = 1 to print_status() only after each test file,
+## not after each expression (verbose = 2),
+## and omit ANSI escapes for a cleaner log
+tinytest::test_package("surveillance", testdir = "testthat",
+                       verbose = 1, color = FALSE)
